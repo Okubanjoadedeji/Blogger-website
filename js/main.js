64d93a9c3b9078eef7479322f826a6305c0d0a24
Okubanjoadedeji <okubanjoadedeji39@gmail.com>
2023-11-09 00:00:21 +0100

    refined

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+const responsive={
+  0:{
+    items:1
+  },
+
+  320:{
+    items:1
+  },
+
+  560:{
+    items:2
+  },
+
+  960:{
+    items:3
+  },
+}
+
 $(document).ready(function(){  
   
   const $nav = $('.nav');
@@ -16,9 +34,18 @@ $(document).ready(function(){
     autoplayTimeout: 3000,
     dots: false,
     nav: true,
-    navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next') ]
-
+    navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+    responsive:responsive,
   });
 
+  //scroll to top
+  $('.move-up span').click(function(){
+    $('html,body').animate({
+      scrollTop:0
+    },2000)
+  })
+ 
+  //AOS 
+  AOS.init();
 
 });
